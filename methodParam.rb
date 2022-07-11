ae8b8802d7288fb016bad90bbbbbed0c5221ed49
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-07-11 20:25:50 +0700

    Create methodParam.rb

---
+++ methodParam.rb
@@ -0,0 +1,22 @@
+puts "Hello World!"
+puts 6+9.0 
+=begin
+ test sj
+=end
+total_satu = 10
+puts total satu
+puts "Totalnya: #{total_satu} "
+def print_hi
+  puts "Re7?"
+end
+print_hi
+def kali(number)
+  puts "#{ number + num}"
+end
+kali(4)
+kali 4
+puts <<-EOL
+Rlxfly
+Re-
+Re7
+EOL
