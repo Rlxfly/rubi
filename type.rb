9272068efafcf008bac295dbbbf6dad2b6cdb9d9
Rell <87053694+Rlxfly@users.noreply.github.com>
2022-08-14 20:35:08 +0700

    Create type.rb
    Signed-off-by: Rell <87053694+Rlxfly@users.noreply.github.com>

---
+++ type.rb
@@ -0,0 +1,10 @@
+print "Banyak item: "
+banyak = gets.to_i
+print "Harga item: "
+harga = gets.to_f
+jumlah = banyak * harga
+puts ""
+print "Totalnya: Rp", jumlah
+
+# Note: gets.to_i = langsung convert ke tipe data intiger
+# sedangkan gets.to_f = convert ke float 
