40e75c4a06b28e8826590ab1b4063164aaaf0304
Rell <me@rlxfly.my.id>
2022-09-08 20:51:45 +0700

    awokawokawok
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ if-boolean-elsif.rb
@@ -0,0 +1,14 @@
+turu = true
+angop = false
+
+if turu and angop
+ puts "Sedang turu tapi angop" # jika keduanya true
+elsif turu and !angop 
+    puts "A mimir" # jika turu = true, dan angop = false
+    elsif !turu and angop #!turu = not turu/turu = false
+        puts "Angop" # jika angop = true, dan turu = false
+else
+    puts "Ngga turu lanjut angop"
+end
+
+# akwowakwoak bingung
