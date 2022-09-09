ba676e32fa868d212b8c7b880ec3229ac3b04393
Rell <me@rlxfly.my.id>
2022-09-09 18:57:05 +0700

    Create class.rb
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ class.rb
@@ -0,0 +1,17 @@
+class Re7
+    attr_accessor :rell, :rlxfly, :re7pntx
+end
+
+sy = Re7.new()
+sy.rell = "Real name"
+sy.rlxfly = "Username GitHub"
+sy.re7pntx = "Awokawok"
+
+gweh = Re7.new()
+gweh.rell = "Iyh"
+gweh.rlxfly = "XD"
+gweh.re7pntx = "Mirip rojak"
+
+puts gweh.rell
+
+# class tsb bisa dipakai untuk beberapa kali
