f17cea5d954ac4813d1208e2108d67c42adbf22e
Rell <me@rlxfly.my.id>
2022-09-09 12:05:33 +0700

    ril or fek
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ case.rb
@@ -0,0 +1,18 @@
+def ha(afh)
+    rilkh = ""
+
+    case afh
+    when "ril"
+        rilkh = "rill coy aseli"
+        when "fek"
+            rilkh = "palesu coy fek"
+        else
+            rilkh = "Yo ndak tau"
+    end
+
+    return rilkh
+end
+
+puts ha("fek")
+
+# peler
