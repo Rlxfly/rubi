c8d89aa117c9afaf3b6c14103897e3798a1eff03
Rell <me@rlxfly.my.id>
2022-09-09 12:48:53 +0700

    suck asw :(
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ eksponen.rb
@@ -0,0 +1,14 @@
+def kya(base, pow)
+    ress = 1
+    pow.times do 
+    ress = ress * base
+end
+
+    return ress
+end
+
+puts kya(10,3) # artinya 10 dipangkatkan 3 kali
+
+# agak bingung tapi paham dikit
+# jadi artinya di pangkatkan
+# keknya :v
