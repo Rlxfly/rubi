5c0f44e707744c680e1477117265a57e792aa5de
Rell <me@rlxfly.my.id>
2022-09-09 11:43:09 +0700

    Create calc2.rb
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ calc2.rb
@@ -0,0 +1,50 @@
+line = "━━━━━━━━━━━━━━━━━━━━━━━"
+l = "「"
+r = "」"
+
+puts "「 ━━━━━━ Calculator ━━━━━━ 」" 
+puts line
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+puts line
+puts "Enter operator: "
+o = gets.chomp()
+puts line
+puts "Enter second number: "
+num2 = gets.chomp().to_f
+
+if o == "+"
+  puts line
+  puts ("Hasilnya: #{num1 + num2}")
+elsif
+  if o == "-"
+   puts line
+  puts ("Hasilnya: #{num1 - num2}")
+    elsif
+  if o == "/"
+   puts line
+  puts ("Hasilnya: #{num1 / num2}")
+    elsif
+  if o == ":"
+    puts line
+  puts ("Hasilnya: #{num1 / num2}")
+    elsif
+  if o == "*"
+    puts line
+  puts ("Hasilnya: #{num1 * num2}")
+    elsif
+  if o == "x"
+    puts line
+  puts ("Hasilnya: #{num1 * num2}")
+  else 
+    puts "Invalid operator"
+  end
+  end
+  end 
+  end 
+  end
+end 
+puts line
+  puts "\n\n\t\tCʀᴇᴀᴛᴇᴅ Bʏ Rʟxғʟʏ"
+
+# kawokowko
