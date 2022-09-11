21f9e9278f5200300bc5aa433eb9c5eb402ba519
Rell <me@rlxfly.my.id>
2022-09-11 21:44:35 +0700

    Create module-require.rb
    Signed-off-by: Rell <me@rlxfly.my.id>

---
+++ module-require.rb
@@ -0,0 +1,15 @@
+module Ntah
+
+    def hi(name)
+        puts "Hello #{name}!"
+    end
+
+    def bye(name)
+        puts "Byee #{name}!"
+    end
+
+    def who(name)
+        puts "You are #{name}~"
+    end
+end
+
