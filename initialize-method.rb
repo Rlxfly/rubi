4442bc4a7116debd5c90462c406dde656dc62415
Rell <me@rlxfly.my.id>
2022-09-11 20:42:23 +0700

    Update initialize-method.rb
    Signed-off-by: Rell <me@rlxfly.my.id>

--- initialize-method.rb
+++ initialize-method.rb
@@ -1,19 +1,23 @@
 class Re7
-    attr_accessor :who, :rlxfly, :age
+    attr_accessor :who, :from, :age
     def initialize(who, from, age)
         @who = who
+        @from = from
+        @age = age
     end
 end
 
-sy = Re7.new("Re7") # jika membuat .new() method maka def initialize akan terpanggil
-sy.who = "Real name"
-sy.from = "Username GitHub"
-sy.age = "Awokawok"
+sy = Re7.new("Re7", "Austria", 15) # jika membuat .new() method maka def initialize akan terpanggil
+#            :who,  :from     :age
+# sy.who = "Real name"
+# sy.from = "Username GitHub" # Tidak dibutuhkan lagi jika sudah memakai calling method/initialize (@anu = anu)
+# sy.age = "Awokawok"
 
-gweh = Re7.new("age")
-gweh.who = "Iyh"
-gweh.from = "XD"
-gweh.age = "Mirip rojak"
+gweh = Re7.new("Rlxfly", "Java", 18)
+# gweh.who = "Iyh"
+# gweh.from = "XD"
+# gweh.age = "Mirip rojak"
 
-
+puts sy.age
+puts gweh.who
 # class tsb bisa dipakai untuk beberapa kali
